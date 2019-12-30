f081608975c5e7eecbaf27e12ba193ffb0efaa13
n-ono <uesugi.4lu.kirari@gmail.com>
2019-12-30 09:36:27 +0900

    初回のコミット

---
+++ src/main/kotlin/com/example/warikan/WarikanApplication.kt
@@ -0,0 +1,11 @@
+package com.example.warikan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarikanApplication
+
+fun main(args: Array<String>) {
+	runApplication<WarikanApplication>(*args)
+}
